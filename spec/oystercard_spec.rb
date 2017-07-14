e3559383360f1613f2accee6cfb8319323e2a01f
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-14 12:46:00 +0100

    Complete first 2 user stories

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  subject(:oystercard) {Oystercard.new}
+  let(:amount) {5}
+
+  it 'shows that there is money on a card' do
+    oystercard.top_up(amount)
+    expect(oystercard.balance).to eq amount
+  end
 end
